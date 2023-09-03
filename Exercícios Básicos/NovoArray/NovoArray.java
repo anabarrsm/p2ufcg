6befc68586e4954f46455699804d9651cc5e958a
Ana Laura <60967531+anabarrsm@users.noreply.github.com>
2023-09-03 15:06:49 -0300

    Delete Exercícios Básicos directory

--- Exercícios Básicos/NovoArray/NovoArray.java
+++
@@ -1,9 +0,0 @@
-public class NovoArray {
-    public static void mult(int[] array, int valor) {
-        for (int i = 0; i < array.length; i++) {
-            array[i] = valor * array[i];
-        }
-        array = new int[10];
-    }
-    mult(novoArray, 10);
-}
